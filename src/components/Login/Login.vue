37a7f3789c9cbf69a0bb263dec2b5616f59957de
quypd <quypd@carpla.vn>
2023-10-10 17:50:48 +0700

    update login task

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,20 +1,53 @@
 <template>
   <div class="bg-fixed h-full flex flex-row justify-center items-center"
        style="background-image: url(/src/assets/images/login/pexels-photo.jpg)">
-    <div class="bg-white w-1/6 h-1/4 rounded-md">
-      <h1 class="p-4 rounded-md">Sign In</h1>
-      <label for="phone" >Nhập số điện thoại của bạn</label>
-      <div>
-        <input name="phone" type="text" class="border-black border">
+    <div class="bg-white rounded-md p-6">
+      <h1 class="p-2 rounded-md text-2xl text-center">Sign In</h1>
+      <div for="phone" class="my-2 font-bold">Số điện thoại</div>
+      <div class="my-2">
+        <input name="phone" type="text" class="border-black border rounded" v-model="phone">
       </div>
-      <button>Đăng nhập</button>
+      <button class="bg-sky-500 mt-2 p-2 rounded w-full" @click="login">Đăng nhập</button>
     </div>
   </div>
 </template>
 
 <script>
+import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      phone: ''
+    }
+  },
+  methods: {
+    login() {
+      const auth = getAuth();
+      console.log(auth)
+      getRedirectResult(auth)
+          .then((result) => {
+            // This gives you a Google Access Token. You can use it to access Google APIs.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+          }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+    }
+  },
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import * as Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 import router from "../router";
+import { initializeApp } from "firebase/app";
 import './assets/css/index.css'
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VUE_APP_FIREBASE_API_KEY || 'AIzaSyBgXVtMkTVYqKDyPk2MnbR4ZXkQ44pYDJs',
+    authDomain: import.meta.env.VUE_APP_FIREBASE_AUTH_DOMAIN || 'test-1a4cc.firebaseapp.com',
+    databaseURL: import.meta.env.VUE_APP_FIREBASE_DATABASE_URL || 'https://test-1a4cc.firebaseio.com',
+    projectId: import.meta.env.VUE_APP_FIREBASE_PROJECT_ID || 'test-1a4cc',
+    storageBucket: import.meta.env.VUE_APP_FIREBASE_STORAGE_BUCKET || 'test-1a4cc.appspot.com',
+    messagingSenderId: import.meta.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID || '772685687500',
+    appId: import.meta.env.VUE_APP_FIREBASE_APP_ID || '1:772685687500:web:5ae7e6d49250335f543069',
+    measurementId: import.meta.env.VUE_APP_FIREBASE_MEASUREMENT_ID || 'G-N6P4XVLF0L'
+};
+
+// Initialize Firebase
+console.log('test')
+const firebase = initializeApp(firebaseConfig);
 
 const app = Vue.createApp(App)
 app.use(router)
+app.use(Vuex)
 app.mount('#app')
