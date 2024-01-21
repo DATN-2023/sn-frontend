4bc7b71a2c8a39b326530a8d7b06c7539d292ef9
quyhot <quyhot1@gmail.com>
2024-01-21 16:29:47 +0700

    update code

--- src/features/components/NotificationList.vue
+++ src/features/components/NotificationList.vue
@@ -131,7 +131,7 @@ export default defineComponent({
               @click="value.onClick(index)">{{ value.title }}
       </button>
     </div>
-    <div class="mt-8 overflow-y-auto h-500px">
+    <div class="mt-8 overflow-y-auto h-400px">
       <NotificationItem  @onClickHasRead="$emit('onClickHasRead')" v-for="item in notifications"
                         class="border-b-1 dark:border-gray-700 py-4" :notification="item"></NotificationItem>
       <InfiniteLoading @infinite="loadItem">
